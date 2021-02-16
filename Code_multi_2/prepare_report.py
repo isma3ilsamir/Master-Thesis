123d3d967b54d7cdccf007dcb16abe0a46433a2a
Amira Lotfy <ALo@vnr.com>
2021-02-17 00:30:17 +0200

    prepare report finalized

--- Code_multi_2/prepare_report.py
+++ Code_multi_2/prepare_report.py
@@ -10,10 +10,16 @@ json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.ends
 
 dfs= []
 for j in json_files:
-    df = pd.read_json(os.path.join(path_to_json, j), lines = True)
+    df = pd.read_json(os.path.join(path_to_json, j))
     dfs.append(df)
 
 report = pd.concat(dfs, axis=0, ignore_index=True)
 
+report = report.sort_values('ts').groupby(['model','dataset','revealed_pct']).tail(1)
+rerun = report[report['success']==False] 
+rerun.to_json('./logs/rerun.json')
+
+successful_runs = report[report['success']==True] 
+successful_runs.to_json('./logs/successful_runs.json')
 import IPython
 IPython.embed()
